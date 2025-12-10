5077f8f950f801de48aa7d21649945b4f8bd4dc4
Turab031 <147339712+Turab031@users.noreply.github.com>
2025-12-10 05:46:23 +0000

    day3

---
+++ basic/day3.js
@@ -0,0 +1,70 @@
+// let arr= [1,2,4,"apple","mango","ch"]
+// console.log(arr)
+
+
+// // shift ->removes first itrm
+// //
+// let nums =[10,20,30];
+// nums.push(40)
+// console.log(nums);
+// nums.pop()
+// console.log(nums);
+
+
+
+// let arr = ["mango","apple","pine","orange","banana"]
+// arr.push("papaya")
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// arr.unshift("kiwi")
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// console.log(arr.includes("mango"))
+
+
+// for( let val of arr){
+//     console.log(val)
+
+// }
+
+// map ()
+
+// let nums = [20,30,40,15,60,70,10]
+// let ans =  nums.map(n=>n*2)
+// console.log(ans);
+
+
+
+// // filter 
+// let num1 = nums.filter(n=>n>20)
+// console.log(num1);
+
+
+// // reduce
+
+// const sum = nums.reduce((acc,val)=>acc+val,0)
+// console.log(sum);
+
+
+let arr  = [2,4,6,8]
+let a1 = arr.map(n=>n*2)
+console.log(a1);
+
+let arr2 = [10,25,30,5,60]
+let a2 = arr.filter(n=>n>20)
+console.log(a2);
+
+let arr3 = [1,2,3,4,5]
+const a3 = arr3.reduce((acc,val)=>acc+val,0)
+console.log(a3);
+
+
+
+
+
+
+
