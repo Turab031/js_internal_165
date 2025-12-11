11aed529dee75db7df02e883dd662e9d2aab383f
Turab031 <147339712+Turab031@users.noreply.github.com>
2025-12-11 05:28:58 +0000

    dates&html

---
+++ basic/day4.js
@@ -0,0 +1,11 @@
+let anydate = new Date()
+
+console.log(anydate.toDateString());
+console.log(anydate.toISOString());
+console.log(anydate.toLocaleDateString());
+console.log(anydate.toTimeString());
+console.log(anydate.toLocaleString());
+
+
+
+
